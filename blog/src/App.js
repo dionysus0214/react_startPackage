a0c595868f0a76ac03e4e1f42a8f36a91ce828e0
dionysus0214 <hiba0214@naver.com>
2022-01-05 22:35:20 +0900

    JSX를 이용해 HTML 페이지 제작

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts = '강남 고기 맛집';
+  function 함수() {
+    return 100;
+  }
+
   return (
+    // 태그에 class를 주고 싶으면 <div className="클래스명">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <h5>{posts}</h5>
+      <p>{함수()}</p>
     </div>
   );
 }
