f8747c27d7b77906d922ef599fdae3ed3c4e6a79
dionysus0214 <hiba0214@naver.com>
2022-01-09 22:29:12 +0900

    state 데이터 변경

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,6 +7,7 @@ function App() {
   // state는 변경되면 HTML이 자동으로 재렌더링 -> 자주 바뀌거나 중요한 데이터는 변수 말고 state로 저장
   // [state 데이터, state 데이터 변경 함수]
   let [title, titleChange] = useState(['강남 맛집 추천', '강남 오마카세', '강남역 데이트']); // ES6 destructuring
+  let [likes, likesChange] = useState(0);
 
   return (
     // 태그에 class를 주고 싶으면 <div className="클래스명">
@@ -14,8 +15,10 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={ () => { titleChange(['성수 맛집 추천', '강남 오마카세', '강남역 데이트']) }}>클릭</button>
       <div className="list">
-        <h3>{ title[0] }</h3>
+        {/* state 변경함수로 변경해야 재렌더링이 잘 일어남 */}
+        <h3>{ title[0] } <span onClick={ () => { likesChange(likes + 1) }}>👍</span> { likes } </h3>
         <p>2월 14일 발행</p>
         <hr/>
       </div>
