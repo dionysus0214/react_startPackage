cd68ec674183f210d8d79e9cc096f57521b0d250
dionysus0214 <hiba0214@naver.com>
2022-01-10 22:16:01 +0900

    spread 연산자로 deep copy

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,8 +6,16 @@ function App() {
   // 문자, 숫자, array, object 모두 저장 가능
   // state는 변경되면 HTML이 자동으로 재렌더링 -> 자주 바뀌거나 중요한 데이터는 변수 말고 state로 저장
   // [state 데이터, state 데이터 변경 함수]
-  let [title, titleChange] = useState(['강남 맛집 추천', '강남 오마카세', '강남역 데이트']); // ES6 destructuring
-  let [likes, likesChange] = useState(0);
+  let [title, changeTitle] = useState(['강남 맛집 추천', '강남 오마카세', '강남역 데이트']); // ES6 destructuring
+  let [likes, changeLikes] = useState(0);
+
+  function clickTitle() {
+    // state의 deep copy해서 수정(원본은 수정 불가, 특히 object랑 array)
+    // deep copy는 reference data type 특징(값 공유 X, 서로 독립적인 값을 가지는 복사)
+    var newArray = [...title];
+    newArray[0] = '판교 멸치쌈밥';
+    changeTitle(newArray);
+  }
 
   return (
     // 태그에 class를 주고 싶으면 <div className="클래스명">
@@ -15,10 +23,10 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ () => { titleChange(['성수 맛집 추천', '강남 오마카세', '강남역 데이트']) }}>클릭</button>
+      <button onClick={ clickTitle }>클릭</button>
       <div className="list">
         {/* state 변경함수로 변경해야 재렌더링이 잘 일어남 */}
-        <h3>{ title[0] } <span onClick={ () => { likesChange(likes + 1) }}>👍</span> { likes } </h3>
+        <h3>{ title[0] } <span onClick={ () => { changeLikes(likes + 1) }}>👍</span> { likes } </h3>
         <p>2월 14일 발행</p>
         <hr/>
       </div>
