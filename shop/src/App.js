fb2506445eb1b9b0f40c20b6cc4cb8a97ef503c3
dionysus0214 <hiba0214@naver.com>
2022-01-28 08:38:12 +0900

    img src mustache tag

--- shop/src/App.js
+++ shop/src/App.js
@@ -52,10 +52,10 @@ function App() {
   );
 }
 
-function ShoesCard(props){
+function ShoesCard(props) {
   return (
     <div className="col-md-4">
-      <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg' } width="100%"/>
+      <img src={ `https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg` } width="100%" />
       <h4>{ props.shoes.title }</h4>
       <p>{ props.shoes.content } & { props.shoes.price }</p>
     </div>
